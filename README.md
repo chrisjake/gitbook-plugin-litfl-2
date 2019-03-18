596965e303d691f227e27e6b74979b79ca8ea4e2
Jake Barlow <jake@jakebarlow.me>
2019-03-18 20:11:55 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gitbook-plugin-litfl-2
+Plugin for gitbook to top nav for Life in the Fast Lane to Gitbook. Based on gitbook-plugin-top-nav. Updated for litfl migration to gutenberg.
+
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    hooks: {},
+
+    blocks: {},
+
+    book: {
+        assets: "./assets",
+        js: [
+          "handler.js"
+        ],
+        css: [
+            "header-element.css"
+        ]
+    }
+};
